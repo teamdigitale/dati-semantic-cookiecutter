bdaf05e0d3ff94472b745675e8aa7dcd4edd5e65
francesco.cardinale <francesco.cardinale@innovaway.it>
2024-03-18 16:32:41 +0100

    updated hooks and setup

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="dati_playground",
+    name="dati_semantic_cookiecutter",
+    version="0.1.0",
     description="Tools to check semantic assets",
+    #url="https://github.com/FrankMaverick/dati-semantic-cookiecutter",
     packages=setuptools.find_packages(),
     install_requires=requirements,
     include_package_data=True,
-    package_data={"": ["data/*.yaml"]},
+    package_data={"": ["scripts/*.yaml"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
