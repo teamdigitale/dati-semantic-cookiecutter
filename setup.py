328aa56b6433596c443dded971637220f1b694fa
francesco.cardinale <francesco.cardinale@innovaway.it>
2024-03-19 12:23:04 +0100

    test check_repo_structure

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-import setuptools
+from setuptools import find_packages
+from setuptools import setup
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setuptools.setup(
+setup(
     name="dati_semantic_cookiecutter",
     version="0.1.0",
     description="Tools to check semantic assets",
-    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],    
+    packages=find_packages('.'),
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "check_repo_structure = dati_semantic_cookiecutter.scripts.check_repo_structure:main",
+            "check_repo_structure = scripts.check_repo_structure:main",
         ]
     },
-    include_package_data=True,
-    package_data={"": ["scripts/*.py"]},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+#    include_package_data=True,
+#    package_data={"": ["scripts/*.py"]},
 )
